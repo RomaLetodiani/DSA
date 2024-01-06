a117abd41038d9dced706299917305a2fc487760
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-06 11:13:33 +0400

    List methods

---
+++ Data Structures/lists.py
@@ -0,0 +1,42 @@
+# https://www.w3schools.com/python/python_lists_methods.asp
+# https://wiki.python.org/moin/TimeComplexity
+
+# O(1)
+# len() - Gets length of the list
+
+# O(1)
+# append() - Adds an element at the end of the list
+
+# O(N)
+# clear() - Removes all the elements from the list
+
+# O(N)
+# copy() - Returns a copy of the list
+
+# O(N)
+# count() - Returns the number of elements with the specified value
+# string[] O(N * M), where M is the length of the string that needs to be counted 
+
+# O(K)
+# extend() - Add the elements of a list (or any iterable), to the end of the current list
+
+# O(N)
+# index() - Returns the index of the first element with the specified value
+# string[] O(N * M), where M is the length of the string that needs to be searched
+
+# O(N)
+# insert() - Adds an element at the specified position
+
+# O(N)
+# pop() - Removes the element at the specified position
+
+# O(N)
+# remove() - Removes the first item with the specified value
+# string[] O(N * M), where M is the length of the string that needs to be removed
+
+# O(N)
+# reverse() - Reverses the order of the list
+
+# O(NlogN)
+# sort() - Sorts the list
+# string[] O(NlogN * M), where M is the length of the longest string

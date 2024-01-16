a2069583fb5619500d84fbb301aee66baa8cb2b9
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-16 13:18:35 +0400

    added queue methods

---
+++ PYTHON/Data Structures/9. queue.py
@@ -0,0 +1,17 @@
+# enqueue(value) - adds an item to the queue;
+# dequeue() - removes an item from the queue. The items are popped in the same order in which they are pushed;
+# front() - get the front item from queue;
+# rear() - get the last item from queue.
+
+# q = []
+# q.append("a") # O(1) @
+# q.append("b") # O(1) @
+# print(q.pop(0)) # O(N)
+# print(q.pop(0)) # O(N)
+
+# from collections import deque
+# q = deque()
+# q.append("a") # O(1)
+# q.append("b") # O(1)
+# print(q.popleft()) # O(1)
+# print(q.popleft()) # O(1)

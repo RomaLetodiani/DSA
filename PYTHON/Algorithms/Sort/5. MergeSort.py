3e148d633f166c10385aca4a1c0298fdeb6beb72
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-12 12:31:00 +0400

    renamed sorting to sort

---
+++ PYTHON/Algorithms/Sort/5. MergeSort.py
@@ -0,0 +1,43 @@
+from arr50K import arr50K
+import time
+
+def merge(arr1, arr2):
+    result = []
+    i = 0
+    j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            result.append(arr1[i])
+            i += 1
+        else:
+            result.append(arr2[j])
+            j += 1
+
+    result.extend(arr1[i:])
+    result.extend(arr2[j:])
+
+    return result
+
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    middle_index = len(arr) // 2
+    left = merge_sort(arr[:middle_index])
+    right = merge_sort(arr[middle_index:])
+
+    return merge(left, right)
+
+# Record the start time
+start_time = time.time()
+
+sorted_arr = merge_sort(arr50K)
+
+# Record the end time
+end_time = time.time()
+
+# It will take at least 0.15 seconds to sort the arr50K
+
+print(sorted_arr)
+print('Length --> ', len(sorted_arr))
+print("Time taken:", end_time - start_time, "seconds")

ea98f64b239893b8c647c3b048f2d03dbcad3cf9
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-06 14:28:59 +0400

    Update 5. MergeSort.js

--- JAVASCRIPT/Algorithms/Sort/5. MergeSort.js
+++ JAVASCRIPT/Algorithms/Sort/5. MergeSort.js
@@ -1,6 +1,6 @@
 const generateArray = require('./ArrayGeneration');
 
-// Generates an array of 500000 numbers from -1000000 to 1000000
+// Generates an array of 500000 random numbers from -1000000 to 1000000
 const arr500K = generateArray(500000, -1000000, 1000000);
 
 const mergeHelper = (arr1, arr2) => {
@@ -40,18 +40,16 @@ const arr = [...arr500K];
 
 // Measure execution time
 const startTime = performance.now();
-// const sortedArr = mergeSort(arr);
-const sortedArr = arr.sort((a, b) => a - b);
-const sortedArrayString = (sortedArr) => {
-  return sortedArr.join(', ');
-};
+const sortedArr = mergeSort(arr);
+// if you want to visualize starting array just change sortedArr to arr500K
+const sortedArrayString = sortedArr.join(', ');
 const endTime = performance.now();
 
-// It will take at least 0.12 seconds to sort the arr500K
+// It will take at least 0.2 seconds to sort the arr500K
 const timeTakenInSeconds = (endTime - startTime) / 1000;
 
-// if you want to visualize starting array just change arr to arr50K
-console.log(sortedArrayString(sortedArr));
+console.log('[', sortedArrayString, ']');
 console.log('--------------------------------------------------------');
-console.log('Length --> ', arr.length);
+console.log('// Stats of Merge Sort //');
+console.log('Array Length --> ', arr.length);
 console.log('Time taken:', timeTakenInSeconds, 'seconds');

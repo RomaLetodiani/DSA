baf1f80aa0ca3e87ecd80b5182f787c91ef73c7b
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-06 12:42:33 +0400

    updated docs

--- PYTHON/Algorithms/Sort/ListGeneration.py
+++ PYTHON/Algorithms/Sort/ListGeneration.py
@@ -4,19 +4,16 @@ def listGenerator(size = 10000, min = -20000, max = 20000):
     """
     Generates a list of random integers within a specified range.
 
-    Parameters:
-        size (int): The size of the list to be generated. Default is 10000.
-        min (int): The minimum value of the random integers to be generated. Default is -20000.
-        max (int): The maximum value of the random integers to be generated. Default is 20000.
-
-    Returns:
-        list: A list of random integers within the specified range.
+    :param size: (int) The size of the list to be generated. Default is 10000.
+    :param min: (int) The minimum value of the random integers to be generated. Default is -20000.
+    :param max: (int) The maximum value of the random integers to be generated. Default is 20000.
+    :return: (list) A list of random integers within the specified range.
     """
     return [random.randint(min, max) for _ in range(size)]
 
 
 # Example usage
-# Generates a list of 50000 numbers from -1000000 to 1000000
+# Generates a list of 50000 random numbers from -1000000 to 1000000
 arr_50k = listGenerator(50000, -1000000, 1000000)
 print(arr_50k)
 print('--------------------------------------------------------')

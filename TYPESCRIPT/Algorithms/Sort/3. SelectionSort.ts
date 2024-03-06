9d6dbe22dbc0a66a792e50d95dc3458ff0ae1a42
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-06 14:32:23 +0400

    Update 3. SelectionSort.ts

--- TYPESCRIPT/Algorithms/Sort/3. SelectionSort.ts
+++ TYPESCRIPT/Algorithms/Sort/3. SelectionSort.ts
@@ -1,5 +1,6 @@
 import { generateArray } from './ArrayGeneration';
 
+// Generates an array of 25000 random numbers from -20000 to 20000
 const arr25K: number[] = generateArray(25000);
 
 const selectionSort = (arr: number[]): void => {
@@ -23,15 +24,15 @@ const arr: number[] = [...arr25K];
 // Measure execution time
 const startTime: number = performance.now();
 selectionSort(arr);
-const sortedArrayString = (arr: number[]): string => {
-  return arr.join(', ');
-};
+const sortedArrayString: string = arr.join(', ');
 const endTime: number = performance.now();
 
-// It will take at least 0.2 milliseconds to sort the arr25K
+// It will take at least 0.2 seconds to sort the arr25K
 const timeTakenInSeconds: number = (endTime - startTime) / 1000;
 
 // if you want to visualize starting array just change arr to arr25K
-console.log(sortedArrayString(arr));
+console.log('[', sortedArrayString, ']');
+console.log('--------------------------------------------------------');
+console.log('// Stats of Selection Sort //');
 console.log('Length --> ', arr.length);
 console.log('Time taken:', timeTakenInSeconds, 'seconds');

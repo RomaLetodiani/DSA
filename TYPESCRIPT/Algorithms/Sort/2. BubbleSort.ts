25e03ab0518bfb72b9dc608ebac04f3216b9a1d8
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-06 14:32:04 +0400

    Update 2. BubbleSort.ts

--- TYPESCRIPT/Algorithms/Sort/2. BubbleSort.ts
+++ TYPESCRIPT/Algorithms/Sort/2. BubbleSort.ts
@@ -1,5 +1,6 @@
 import { generateArray } from './ArrayGeneration';
 
+// Generates an array of 25000 random numbers from -20000 to 20000
 const arr25K: number[] = generateArray(25000);
 
 const bubbleSort = (arr: number[]) => {
@@ -22,17 +23,15 @@ const arr: number[] = [...arr25K];
 // Measure execution time
 const startTime: number = performance.now();
 bubbleSort(arr);
-const sortedArrayString = (arr: number[]): string => {
-  return '[ ' + arr.join(', ') + ' ]';
-};
+// if you want to visualize starting array just change arr to arr25K
+const sortedArrayString: string = arr25K.join(', ');
 const endTime: number = performance.now();
 
 // It will take at least 0.7 seconds to sort the arr25K
 const timeTakenInSeconds: number = (endTime - startTime) / 1000;
 
-// if you want to visualize starting array just change arr to arr25K
-console.log(sortedArrayString(arr));
+console.log('[', sortedArrayString, ']');
 console.log('--------------------------------------------------------');
-console.log('//Stats of Bubble Sort//');
+console.log('// Stats of Bubble Sort //');
 console.log('Length --> ', arr.length);
 console.log('Time taken:', timeTakenInSeconds, 'seconds');

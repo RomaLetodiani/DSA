447abd4db682dad0d7288550ca8fa42c12996c93
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-06 14:32:46 +0400

    fixed example usage error

--- TYPESCRIPT/Algorithms/Sort/ArrayGeneration.ts
+++ TYPESCRIPT/Algorithms/Sort/ArrayGeneration.ts
@@ -9,12 +9,15 @@ export const generateArray = (size: number = 10000, min: number = -20000, max: n
   return Array.from({ length: size }, () => Math.floor(Math.random() * (max - min + 1)) + min);
 };
 
-// Example usage
-// Generates an array of 50000 random numbers from -1000000 to 1000000
-const arr50K: number[] = generateArray(50000, -1000000, 1000000);
-console.log('[', arr50K.join(', '), ']');
-console.log('--------------------------------------------------------');
-console.log('Array Length:', arr50K.length);
+// Checking if require.main === module to prevent running the example usage in other files
+if (require.main === module) {
+  // Example usage
+  // Generates an array of 50000 random numbers from -1000000 to 1000000
+  const arr50K: number[] = generateArray(50000, -1000000, 1000000);
+  console.log('[', arr50K.join(', '), ']');
+  console.log('--------------------------------------------------------');
+  console.log('Array Length:', arr50K.length);
+}
 
 // Run this command in the terminal
 // ts-node '.\Algorithms\Sort\ArrayGeneration.ts'

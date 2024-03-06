02ed40fcfcf9e2e301ae747ab79a40829cd63a21
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-06 14:28:55 +0400

    Update 3. SelectionSort.js

--- JAVASCRIPT/Algorithms/Sort/3. SelectionSort.js
+++ JAVASCRIPT/Algorithms/Sort/3. SelectionSort.js
@@ -1,6 +1,6 @@
 const generateArray = require('./ArrayGeneration');
 
-// Generates an array of 25000 numbers from -20000 to 20000
+// Generates an array of 25000 random numbers from -20000 to 20000
 const arr25K = generateArray(25000);
 
 const selectionSort = (arr) => {
@@ -24,16 +24,15 @@ const arr = [...arr25K];
 // Measure execution time
 const startTime = performance.now();
 selectionSort(arr);
-const sortedArrayString = (arr) => {
-  return arr.join(', ');
-};
+// if you want to visualize starting array just change arr to arr25K
+const sortedArrayString = arr.join(', ');
 const endTime = performance.now();
 
 // It will take at least 0.2 seconds to sort the arr25K
 const timeTakenInSeconds = (endTime - startTime) / 1000;
 
-// if you want to visualize starting array just change arr to arr25K
-console.log(sortedArrayString(arr));
+console.log('[', sortedArrayString, ']');
 console.log('--------------------------------------------------------');
-console.log('Length --> ', arr.length);
+console.log('// Stats of Selection Sort //');
+console.log('Array Length --> ', arr.length);
 console.log('Time taken:', timeTakenInSeconds, 'seconds');

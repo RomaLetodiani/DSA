e97fdf04fe151853aa005561ccdf2c2ada36dd3c
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-06 12:54:09 +0400

    fixed example usage error

--- JAVASCRIPT/Algorithms/Sort/ArrayGeneration.js
+++ JAVASCRIPT/Algorithms/Sort/ArrayGeneration.js
@@ -11,12 +11,15 @@ const generateArray = (size = 10000, min = -20000, max = 20000) => {
 
 module.exports = generateArray;
 
-// Example usage
-// Generates an array of 50000 random numbers from -1000000 to 1000000
-const arr50K = generateArray(50000, -1000000, 1000000);
-console.log('[', arr50K.join(', '), ']');
-console.log('--------------------------------------------------------');
-console.log('Array Length', arr50K.length);
+// Checking if require.main === module to prevent running the example usage in other files
+if (require.main === module) {
+  // Example usage
+  // Generates an array of 50000 random numbers from -1000000 to 1000000
+  const arr50K = generateArray(50000, -1000000, 1000000);
+  console.log('[', arr50K.join(', '), ']');
+  console.log('--------------------------------------------------------');
+  console.log('Array Length', arr50K.length);
+}
 
 // Run this command in the terminal
 // node '.\Algorithms\Sort\ArrayGeneration.ts'

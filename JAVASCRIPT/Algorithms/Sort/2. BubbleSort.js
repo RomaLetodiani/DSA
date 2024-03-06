a930ff56ed0e4023c722e610ea8690368b7c866b
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-06 14:28:52 +0400

    Update 2. BubbleSort.js

--- JAVASCRIPT/Algorithms/Sort/2. BubbleSort.js
+++ JAVASCRIPT/Algorithms/Sort/2. BubbleSort.js
@@ -1,6 +1,6 @@
 const generateArray = require('./ArrayGeneration');
 
-// Generates an array of 25000 numbers from -20000 to 20000
+// Generates an array of 25000 random numbers from -20000 to 20000
 const arr25K = generateArray(25000);
 
 const bubbleSort = (arr) => {
@@ -23,16 +23,15 @@ const arr = [...arr25K];
 // Measure execution time
 const startTime = performance.now();
 bubbleSort(arr);
-const sortedArrayString = (arr) => {
-  return arr.join(', ');
-};
+// if you want to visualize starting array just change arr to arr25K
+const sortedArrayString = arr.join(', ');
 const endTime = performance.now();
 
 // It will take at least 0.7 seconds to sort the arr25K
 const timeTakenInSeconds = (endTime - startTime) / 1000;
 
-// if you want to visualize starting array just change arr to arr25K
-console.log(sortedArrayString(arr));
+console.log('[', sortedArrayString, ']');
 console.log('--------------------------------------------------------');
-console.log('Length --> ', arr.length);
+console.log('// Stats of Bubble Sort //');
+console.log('Array Length --> ', arr.length);
 console.log('Time taken:', timeTakenInSeconds, 'seconds');

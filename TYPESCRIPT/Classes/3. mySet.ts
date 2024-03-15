2623111f6e2634acedb21d225fd9c656434ddbab
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-15 17:51:16 +0400

    Update 3. mySet.ts

--- TYPESCRIPT/Classes/3. mySet.ts
+++ TYPESCRIPT/Classes/3. mySet.ts
@@ -1,14 +1,4 @@
-// Mixing Operations: You might perform additional bitwise operations
-// (like XOR or bit shifting) between the characters to achieve
-// a better mixing of bits, ensuring a different hash code for similar strings.
-
-const myHash = (string: string) => {
-  let hash: number = 0;
-  for (let i: number = 0; i < string.length; i++) {
-    hash = (hash << 5) ^ (hash >> 27) ^ string.charCodeAt(i);
-  }
-  return hash & 0x7fffffff;
-};
+import myHash from './4. myHash';
 
 class MySet {
   buckets: string[][];
